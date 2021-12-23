41325563a8bec2104094c7299516bf49cf13528c
Muhamed Didovic <muhamed.didovic@gmail.com>
2021-12-23 12:01:30 +0100

    update parser to comply with changes

--- src/Parser.php
+++ src/Parser.php
@@ -10,11 +10,11 @@ class Parser
      * @var Crawler
      */
     public $crawler;
-
+    
     public $jsfunction;
-
+    
     public $lessons;
-
+    
     /**
      * Parse HTML.
      *
@@ -25,23 +25,25 @@ class Parser
     public function parse($html)
     {
         $this->lessons = $html;
-
+        
         return $this;
     }
-
+    
     public function getPage()
     {
         $lessons = [];
         foreach ($this->lessons->data as $lesson) {
-            $lesson = (object) [
-                'link' => "https://codecourse.com/api/parts/" . $lesson->id . "/download",
-                'title' =>$lesson->title,
-                'slug' => $lesson->slug,
-                'filename' => sprintf('%02d', (int) $lesson->order - 1) . '-' . $lesson->slug. '.mp4',
+            $lesson    = (object)[
+                'link'        => "https://codecourse.com/api/parts/" . $lesson->id . "/download",
+                'title'       => $lesson->title,
+                'slug'        => $lesson->slug,
+                'filename'    => sprintf('%02d', (int)$lesson->order - 1) . '-' . $lesson->slug . '.mp4',
+                'provider_id' => $lesson->video->vimeo_id ?? null, //$lesson->file->data->stream->data->provider_id ?? null,
+                'download'    => $lesson->file->data->download ?? null,
             ];
             $lessons[] = $lesson;
         }
-
+        
         return $lessons;
     }
 }
